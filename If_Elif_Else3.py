5072afa15f81aa42ac1d63bab9fb1a6daca052b2
Taylor Tackett <tm.96@live.com>
2025-07-19 09:48:41 -0400

    for sale and online

--- If_Elif_Else3.py
+++ If_Elif_Else3.py
@@ -1,5 +1,27 @@
+"""
 name = input("Enter your name: ")
 if name == "":
     print("You did not type in your name!")
 else:
     print(f"Hello {name}!")
+"""
+
+"""
+for_sale = False
+
+if for_sale:
+    print("The item is for sale.")
+
+else:
+    print("The item is not for sale.")
+"""
+
+online = False
+
+if online:
+    print("The user is online.")
+
+else:
+    print("The user is offline.")
+
+
